649eab9c654733ea7f6c44dbe4a0b7c6fb15a4c8
Gilbert <gssenyonjo@gmail.com>
2022-05-03 00:04:17 +0300

    Initial commit

---
+++ src/main/java/net/passioncloud/HelloHandler.java
@@ -0,0 +1,29 @@
+package net.passioncloud;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+import java.util.Map;
+
+public class HelloHandler implements RequestHandler<Map<String,String>,String> {
+    // static code will be reused between invocations
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        String response = "200 OK here";
+        // log execution
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+        return response;
+    }
+}
